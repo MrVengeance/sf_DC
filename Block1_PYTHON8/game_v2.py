629b352811b200a9159a83c63adcdf178f4cd046
MeVengeance <zanyato.vse@gmail.com>
2022-06-17 21:44:39 +0300

    fix Pep8

--- Block1_PYTHON8/game_v2.py
+++ Block1_PYTHON8/game_v2.py
@@ -4,32 +4,33 @@
 
 import numpy as np
 
-def random_predict(number:int=np.random.randint(1, 101)) -> int:
+
+def random_predict(number: int=np.random.randint(1, 101)) -> int:
     """Рандомно угадываем число
-
     Args:
-        number (int, optional): Загаданное число. По умолчанию рандомно загадывается компьютером в диапазоне 1-100.
-
+        number: Загаданное число. По умолчанию рандомно загадывается \
+            компьютером в диапазоне 1-100.
     Returns:
-        int: Число попыток
+        int: Число попыток и само число
     """
 
     count = 0
-    mn,mx = 1,100
+    mn, mx = 1, 100
 
     while True:
         count += 1
         md = int((mn+mx)/2)
-        
+
         if md > number:
-            mx = md        
+            mx = md
         elif md < number:
             mn = md
 
         else:
-            print(f"Компьютер угадал число за {count} попыток. Это число {number}")
-            break #конец игры выход из цикла
-                   
+            print(f"Компьютер угадал число за {count} попыток. \
+                Это число {number}")
+            break  # End of game. Exit from while
+
     return count
 
 random_predict()
