e881ff87876fe6497bb9de3c9e2dbed41f11eb0a
MeVengeance <zanyato.vse@gmail.com>
2022-11-17 00:01:51 +0300

    new_lessons

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-import pandas as pd
-
-print(pd.__name__)
+import urllib.request, re, ssl
+s = urllib.request.urlopen("https://172.17.8.121:8080/login?username=Admin&password=12345", context=ssl._create_unverified_context()).read()
+for x in s.replace("\n", "").split("{"):
+    m = re.search('"name"\s*\:\s*"(.*?)".*Channel', x)
+    if not m: continue
+    print (m.group(1))
+    
---
+++ testtttt.py
@@ -0,0 +1,4 @@
+a = [1,2,3]
+b = a
+c = [1,2,3]
+print(a == b, a == c, a is b, a is c)
