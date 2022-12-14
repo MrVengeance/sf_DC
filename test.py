d48617dc0385d16da0bc1b52c856558a97eaa756
MeVengeance <zanyato.vse@gmail.com>
2022-12-14 13:33:53 +0300

    new

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import urllib.request, re, ssl
-s = urllib.request.urlopen("https://172.17.8.121:8080/login?username=Admin&password=12345", context=ssl._create_unverified_context()).read()
+s = urllib.request.urlopen("https://172.17.8.127:8080/login?username=Admin&password=12345", context=ssl._create_unverified_context()).read()
 for x in s.replace("\n", "").split("{"):
     m = re.search('"name"\s*\:\s*"(.*?)".*Channel', x)
     if not m: continue
